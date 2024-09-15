1487c281117e8523a72237bccd0c17991e291cf7
FCTL3314 <solovev.nikita.05@gmail.com>
2024-09-15 23:59:19 +0300

    Added Workout model DTO's for requests

--- internal/domain/workout.go
+++ internal/domain/workout.go
@@ -5,12 +5,39 @@ import (
 )
 
 type Workout struct {
-	ID          uint
+	ID          int64
 	Name        string
 	Description string
-	UserID      uint
+	UserID      int64
 	User        *User
 	Exercises   []*Exercise `gorm:"many2many:workout_exercises;"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
+}
+
+type CreateWorkoutRequest struct {
+	Name        string `json:"name" binding:"required,min=1,max=128"`
+	Description string `json:"description" binding:"required,min=1,max=256"`
+}
+
+type UpdateWorkoutRequest struct {
+	Name        *string `json:"name" binding:"required,min=1,max=128"`
+	Description *string `json:"description" binding:"required,min=1,max=256"`
+}
+
+func NewWorkoutFromCreateRequest(req *CreateWorkoutRequest) *Workout {
+	return &Workout{
+		Name:        req.Name,
+		Description: req.Description,
+	}
+}
+
+func (w *Workout) ApplyUpdate(req *UpdateWorkoutRequest) {
+	if req.Name != nil {
+		w.Name = *req.Name
+	}
+
+	if req.Description != nil {
+		w.Description = *req.Description
+	}
 }
