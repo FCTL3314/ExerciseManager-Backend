daff60a03f76bc72e80d0734be0d72c8370724f9
FCTL3314 <solovev.nikita.05@gmail.com>
2024-09-20 18:13:12 +0300

    Final refactor / Testing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"ExerciseManager/api/router"
 	"ExerciseManager/bootstrap"
+	"fmt"
 	"log"
 )
 
@@ -11,6 +12,7 @@ func main() {
 
 	router.RegisterRoutes(app.Router, app.DB, app.Cfg, app.LoggerGroup)
 
+	fmt.Printf("Listening and serving HTTP on %s\n", app.Cfg.Server.Address)
 	if err := app.Router.Run(app.Cfg.Server.Address); err != nil {
 		log.Fatal(err)
 	}
